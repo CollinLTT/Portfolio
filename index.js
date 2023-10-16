83869b3cd87f78907dcad1e983890ed2f5945ec8
Collin Trump <ctrump9912@sdsu.edu>
2023-10-16 02:40:26 -0700

    Created header links and new html pages.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const track = document.getElementById("image-track");
 const flack = document.getElementById("image-flack");
-const title = document.getElementById("image-title");
+//const title = document.getElementById("image-title");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
@@ -31,11 +31,6 @@ const handleOnMove = e => {
     transform: `translate(${nextPercentage}%, 51%)`
   }, { duration: 1200, fill: "forwards" });
 
-  /*Scrolls title image*/
-  title.animate({
-    transform: `translate(${nextPercentage}%, 20%)`
-  }, { duration: 1200, fill: "forwards" });
-
   /*Changes what part of the photo you see*/
   for (const image of track.getElementsByClassName("image")) {
     image.animate({
@@ -48,13 +43,6 @@ const handleOnMove = e => {
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
-  }
-
-  /*Changes title elements*/
-  for (const image of title.getElementsByClassName("image")) {
-
-    /*Changes title opacity based on distance scrolled*/
-    title.style.opacity = `${((100 + nextPercentage) / 100)}`;
   }
 
   /*Moves flack images inside of standard images + lowers opacity by scroll distance*/
@@ -80,13 +68,6 @@ const handleOnMove = e => {
       */
   /*--------------------------------------End section----------------------------------------*/
 
-  /*Moves title image back to starting position*/
-  if (((100 + nextPercentage) / 100) >= 0.95) {
-    title.animate({
-      transform: `translate(0%, 60%)`
-    }, { duration: 1200, fill: "forwards" });
-
-  }
 }
 
 window.onmousedown = e => handleOnDown(e);
