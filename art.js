56c6356aeb7eae8911a8885aea1e0a43b9269d4e
Collin Trump <ctrump9912@sdsu.edu>
2023-10-28 02:03:03 -0700

    Sorted files.

--- art.js
+++
@@ -1,9 +0,0 @@
-const image = document.getElementById("enlarge-image");
-
-image.addEventListener("mouseover", () => {
-    image.style.transform = "scale(1.2)"; // Increase the scale to make it bigger
-});
-
-image.addEventListener("mouseout", () => {
-    image.style.transform = "scale(1)"; // Revert to the original size
-});
